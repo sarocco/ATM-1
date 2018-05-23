a05bee3b79bc10c851fa6b84226aed3d03b50328
Carolina Rocco <Caro@macbook-pro-de-carolina.local>
2018-05-23 17:49:04 -0300

    1st commit

---
+++ ATM-1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ATM-1
+//
+//  Created by Carolina Rocco on 23/5/18.
+//  Copyright © 2018 Silvia Rocco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
